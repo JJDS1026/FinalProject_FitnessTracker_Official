dfdeed207cc74c60121c6474389b6150f08a205f
Lesley Black Velvet <81901006+Lesley-Black-Velvet@users.noreply.github.com>
2023-07-18 15:27:37 +0800

    Added initial values

--- app/src/main/java/com/example/group6/finalproject_fitnesstracker/User.java
+++ app/src/main/java/com/example/group6/finalproject_fitnesstracker/User.java
@@ -1,4 +1,17 @@
 package com.example.group6.finalproject_fitnesstracker;
 
-public class User {
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class User extends RealmObject
+{
+    @PrimaryKey
+    private String uuid;
+
+    private String username;
+
+    private String password;
+
+
+    
 }
