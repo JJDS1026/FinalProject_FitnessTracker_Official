acd90b8269d5c5f855648e414e5e132c3733e8eb
Lesley Black Velvet <81901006+Lesley-Black-Velvet@users.noreply.github.com>
2023-07-19 14:52:47 +0800

    Updated Values.

--- app/src/main/java/com/example/group6/finalproject_fitnesstracker/LoginActivity.java
+++ app/src/main/java/com/example/group6/finalproject_fitnesstracker/LoginActivity.java
@@ -3,11 +3,19 @@ package com.example.group6.finalproject_fitnesstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity
 public class LoginActivity extends AppCompatActivity {
+
+    @ViewById
+    EditText usernamereg;
+
+    @ViewById
+    EditText userpasswordreg;
 
 
 
--- app/src/main/java/com/example/group6/finalproject_fitnesstracker/User.java
+++ app/src/main/java/com/example/group6/finalproject_fitnesstracker/User.java
@@ -8,10 +8,92 @@ public class User extends RealmObject
     @PrimaryKey
     private String uuid;
 
+    // Basic User Information
     private String username;
-
     private String password;
+    private String description;
 
+    // Health Information
+    private String realname;
+    private float height;
+    private float weight;
+    private float bmi;
 
-    
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(float bmi) {
+        this.bmi = bmi;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uuid='" + uuid + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", description='" + description + '\'' +
+                ", realname='" + realname + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", bmi=" + bmi +
+                '}';
+    }
 }
